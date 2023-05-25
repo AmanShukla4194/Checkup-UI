e9b452405372f35c82332db8137b659d7f4374f3
Aman Shukla <118071759+AmanShukla4194@users.noreply.github.com>
2023-05-25 14:03:29 +0530

    Add files via upload

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,18 +2,29 @@ import React from 'react';
 import './navbar.css';
 import Vector1 from '../../assests/Vector1.png';
 import Checkup from '../../assests/Checkup.png';
+import bell from '../../assests/bell.png';
+import Ellipse1 from '../../assests/Ellipse1.png';
+import Ellipse2 from '../../assests/Ellipse2.png';
+import Doctor from '../../assests/Doctor.png';
+
+
 
 const Navbar = () => {
   return (
     <div>
-<nav className="navbar navbar-light bg-light">
-  <a className="navbar-brand" href="/">
-    <img src={Vector1} width="60" height="50" alt="logo"/>
-    <img src={Checkup} width="100" height="50" alt="logo"/>
+<nav className="navbar">
+  <a  href="/">
+    <img src={Vector1}  alt="logo" className="vector"/>
+    <img src={Checkup} alt="Checkup" className="Checkup"/>
+    <img src={bell}  alt="Bell" className="Bell"/>
+    <img src={Ellipse1} alt="RedDot" className="Ellipse1"/>
+    <img src={Ellipse2} alt="Profile" className="Ellipse2"/>
+    <img src={Doctor} alt="doctor" className="Doctor"/>
   </a>
 </nav>
-    </div>
+</div>
   )
 }
 
 export default Navbar
+
