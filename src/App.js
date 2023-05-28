0747ddf02052ff78773d6f3d7aaebb4363cefb2f
Aman Shukla <118071759+AmanShukla4194@users.noreply.github.com>
2023-05-28 12:11:51 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 // import Navbar from './components/navbar/Navbar'  for not doing it all separate
-import {Navbar} from './components';
+import {Navbar, Sidebar, First} from './components';
 import './App.css'
 
 function App() {
   return (
-   <div className="App">
-    <div className="gradient__bg">
-      <Navbar/>
-    </div>
-
+    <div>
+      <First/>
+      {/* <Navbar/>
+      <Sidebar/> */}
    </div>
   );
 }
