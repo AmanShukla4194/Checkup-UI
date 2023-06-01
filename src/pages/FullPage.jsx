659a16135b976f740a97e2dcc56dfc834a4f9476
Aman Shukla <118071759+AmanShukla4194@users.noreply.github.com>
2023-06-02 00:01:23 +0530

    Add files via upload

---
+++ src/pages/FullPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Navbar, Sidebar } from '../components'
+
+function Home() {
+  return (
+    <>
+        <Navbar/>
+        <Sidebar/>
+      
+    </>
+  )
+}
+
+export default Home
